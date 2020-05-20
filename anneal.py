139939965112825006edf8dc61e5db8314840f9b
Jade Chongsathapornpong <jade.chongsathapornpong@gmail.com>
2020-05-19 17:47:01 -0700

    Update anneal.py
    handled edges

--- anneal.py
+++ anneal.py
@@ -3,23 +3,54 @@ import matplotlib.pyplot as pl
 
 #Inputs
 N = 5
-fill = 7
+fill = 14
 
 N2 = N**2
 frac = fill/N2
 if frac > 1.0:
     print("lattice overfill")
     quit()
-if frac < 0.333: x = 3
-elif frac < 0.5: x = 2
-else: x = 1
+if frac < 0.333: a = 3
+elif frac < 0.5: a = 2
+else: a = 1
 
 #Initial Lattice Condition
 lat = np.zeros(N2, dtype=int)
 for i in range(fill):
-    lat[N2-1-x*i] = 1
+    lat[N2-1-a*i] = 1
 lat = lat.reshape((N,N))
+print(lat)
 
-#Adjust
+#Functions
+def checkAdjacents(i,j,lat):
+    adj = np.array([])
+    try:
+        #Top edge
+        if i == 0:
+            adj = np.array([lat[i,j-1] , lat[i,j+1] , lat[i+1,j-1] , lat[i+1,j]])
+        #Left edge
+        elif j == 0:
+            adj = np.array([lat[i-1,j] , lat[i-1,j+1] , lat[i,j+1] , lat[i+1,j]])
+        #Bottom edge
+        elif i == N-1:
+            adj = np.array([lat[i-1,j] , lat[i-1,j+1] , lat[i,j-1] , lat[i,j+1]])
+        #Right edge
+        elif j == N-1:
+            adj = np.array([lat[i-1,j] , lat[i,j-1] , lat[i+1,j-1] , lat[i+1,j]])
+        else:
+        #Check all adjacents
+            adj = np.array([lat[i-1,j] , lat[i-1,j+1] , lat[i,j-1] , lat[i,j+1] , lat[i+1,j-1] , lat[i+1,j]])
+    except IndexError:
+        print("Corner i"+str(i)+", j"+str(j))
+        adj = np.array([])
+    return adj
+
+#Main
 T = 1.0
-print(lat[3,3])
+for i in range(N):
+    for j in range(N):
+        if lat[i,j] == 1:
+            adj = checkAdjacents(i,j,lat)
+            for adjacent in adj:
+                if adjacent == 0:
+                    #do something here to checkadjacents for the i,j of the corresponding adjacent
